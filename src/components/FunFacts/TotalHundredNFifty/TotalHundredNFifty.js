f28823ab839c337f6345559edc1f75cdb11828f6
Rhythm <rrhythmsharma@gmail.com>
2019-10-29 02:58:21 +0530

    Added TotalHundredNFifty component

---
+++ src/components/FunFacts/TotalHundredNFifty/TotalHundredNFifty.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import rawData from  '../../../assets/sachin.json';
+
+const TotalHundredNFifty = () => {
+    
+        let hundredCounter = 0;
+        let fiftyCounter = 0; 
+
+        rawData.forEach((item) => {
+            
+            if(typeof(item.batting_score) === 'string'){
+                if(item.batting_score.charAt(item.batting_score.length - 1) === "*"){
+                    if(parseInt(item.batting_score.slice(0, -1), 10) >= 100 ){
+                        hundredCounter++;
+                    }
+                    if(parseInt(item.batting_score.slice(0, -1), 10) >= 50  && parseInt(item.batting_score.slice(0, -1), 10) <= 99 ){
+                        fiftyCounter++;
+                    }
+                }
+            }else{
+                if(item.batting_score >= 100){
+                    hundredCounter++
+                }
+                if(item.batting_score >= 50  && item.batting_score <= 99 ){
+                    fiftyCounter++
+                }
+            }
+        })
+
+
+    return (
+        <div className="fact-container-1">
+            <h3 className="fact-heading">Total Hundreds</h3>
+            <div className="wrapper">
+                <div className="right"></div>
+                <div className="left"></div>
+                    <div className="middle">
+                        <p>{hundredCounter}</p>
+                    </div>
+                <div className="popover"></div>
+            </div>
+            <h3 className="fact-heading">Total Fifty</h3>
+            <div className="wrapper">
+                <div className="right"></div>
+                <div className="left"></div>
+                    <div className="middle">
+                        <p>{fiftyCounter}</p>
+                    </div>
+                <div className="popover"></div>
+            </div>
+        </div>
+    )
+}
+
+export default TotalHundredNFifty; 
