d94556b3bbca3adcd09e2d54c7b3d9b3270b2e29
Rhythm <rrhythmsharma@gmail.com>
2019-10-29 02:58:44 +0530

    Added TotalCatches component

---
+++ src/components/FunFacts/TotalCatches/TotalCatches.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import rawData from  '../../../assets/sachin.json';
+
+
+const TotalCatches = () => {
+
+    const CalculateTotalCatches = () => {
+
+        const Catches = rawData.map((item) => {
+            let catches;
+            if(typeof(item.catches) === 'string'){
+                catches = null;
+            }else{
+                catches = item.catches
+            }
+            return (catches)
+        })
+
+        console.log(Catches)
+
+        let sum = Catches.reduce((a, b) => a + b, 0)
+        return sum
+    }
+
+    return(
+        <div className="fact-container-1">
+            <h3 className="fact-heading">Average Score of Sachin When India Won</h3>
+            <div className="wrapper">
+                <div className="right"></div>
+                <div className="left"></div>
+                    <div className="middle">
+                        <p>{CalculateTotalCatches()}</p>
+                    </div>
+                <div className="popover"></div>
+            </div>
+        </div>
+)
+}
+
+export default TotalCatches;
